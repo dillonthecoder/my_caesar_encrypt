ccdc1ef5cb590e97e8d71c8a988d206b5358fadc
Dillon Stone <maitreyaco@gmail.com>
2022-06-08 14:48:30 -0400

    initial commit

---
+++ caesar_encrypt.py
@@ -0,0 +1,17 @@
+import string
+
+
+def caesar(text, shift, alphabets):
+
+    def shift_alphabet(alphabet):
+        return alphabet[shift:] + alphabet[:shift]
+
+    shifted_alphabets = tuple(map(shift_alphabet, alphabets))
+    final_alphabet = ''.join(alphabets)
+    final_shifted_alphabet = ''.join(shifted_alphabets)
+    table = str.maketrans(final_alphabet, final_shifted_alphabet)
+    return text.translate(table)
+
+
+plain_text = 'Maitreya Studios makes the best NFT projects!'
+print(caesar(plain_text, 7, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation]))
